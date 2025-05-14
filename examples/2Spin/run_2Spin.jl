d7ef3b47adf3c9f762fafda145b0c37b599e7f3a
Mattia Tarabolo <mattia.tarabolo@gmail.com>
2025-05-14 09:16:56 +0200

    Refactor code structure for improved readability and maintainability

--- examples/2Spin/run_2Spin.jl
+++ examples/2Spin/run_2Spin.jl
@@ -8,13 +8,45 @@ using LaTeXStrings
 rcParams = PyDict(plt.matplotlib["rcParams"])
 rcParams["font.size"] = 8
 
-function run_Bim_RRG(N, K, J, D, tmax, nsim)
+function run_Bin_RRG_single_nodes(N, K, J, D, tmax, nsim, dt, dt_save, ilist)
     rng = Xoshiro(1234)
 
     x0_min, x0_max = -3.0, 3.0
-    dt = 1e-3
+    
+    tsave = collect(range(0, tmax; step=dt_save))
 
-    tsave = collect(range(0, tmax; step=1e-1))
+    # Define ensemble model
+    model_ensemble = TwoSpinModelRRG_Bim(N, K, J, D)
+
+    # Sample ensemble
+    _, traj_alls, _ = sample_ensemble_2Spin(model_ensemble, x0_min, x0_max, tmax, tsave, nsim; rng=rng, showprogress=true, dt=dt)
+    
+    
+    # Compute autocorrelation function for 2 nodes only
+    trajs = [zeros(nsim, length(tsave)) for _ in 1:length(ilist)]
+    autocorrs = Vector{Matrix{Float64}}(undef, length(ilist))
+    tidxs = Vector{Vector{Int}}(undef, length(ilist))
+    for (idx, i) in enumerate(ilist)
+        @inbounds for isim in 1:nsim
+            trajs[idx][isim, :] .= traj_alls[isim][i, :]
+        end
+        autocorrs[idx], tidxs[idx] = compute_autocorr(trajs[idx]; showprogress=true)
+    end
+    
+    # Save results
+    isdir("data") || mkdir("data")
+    isdir("data/RRG") || mkdir("data/RRG")
+
+    filename = "data/RRG/Corr_Bim_N-$(N)_K-$(K)_J-$(J)_D-$(D)_tmax-$(tmax)_dtMC-$(dt)_dtsave-$(dt_save)_nsim-$(nsim)_x0min-$(x0_min)_x0max-$(x0_max).jld2"
+    jldsave(filename; tsave, tidxs, autocorrs)
+end
+
+function run_Bim_RRG(N, K, J, D, tmax, nsim, dt, dt_save)
+    rng = Xoshiro(1234)
+
+    x0_min, x0_max = -3.0, 3.0
+    
+    tsave = collect(range(0, tmax; step=dt_save))
 
     # Define ensemble model
     model_ensemble = TwoSpinModelRRG_Bim(N, K, J, D)
@@ -23,76 +55,24 @@ function run_Bim_RRG(N, K, J, D, tmax, nsim)
     _, traj_alls, _ = sample_ensemble_2Spin(model_ensemble, x0_min, x0_max, tmax, tsave, nsim; rng=rng, showprogress=true, dt=dt)
 
     # Compute correlation function
-    autocorr_traj, tidxs = compute_autocorr(traj_alls)
+    autocorr_traj, tidxs = compute_autocorr(traj_alls; showprogress=true)
     tvec = tsave[tidxs]
-
-    # Compute autocorrelation function for 2 nodes only
-    traj_1 = zeros(nsim, length(tsave))
-    traj_2 = zeros(nsim, length(tsave))
-    @inbounds for i in 1:nsim
-        traj_1[i, :] = view(traj_alls[i], 1, :)
-        traj_2[i, :] = view(traj_alls[i], 2, :)
-    end
-    autocorr_traj_1, tidxs_1 = compute_autocorr(traj_1)
-    autocorr_traj_2, tidxs_2 = compute_autocorr(traj_2)
-
+    
     # Save results
     isdir("data") || mkdir("data")
     isdir("data/RRG") || mkdir("data/RRG")
 
-    filename = "data/RRG/Corr_N$(N)_K$(K)_J$(J)_D$(D)_tmax$(tmax)_dtMC-$(dt)_dtsave-1e-1_nsim$(nsim)_x0min-$(x0_min)_x0max-$(x0_max).jld2"
-    jldsave(filename; autocorr_traj, tsave, tidxs, autocorr_traj_1, tidxs_1, autocorr_traj_2, tidxs_2)
-
-    # Plot correlations
-    tws = collect(10 .^ range(log10(1.0), stop=log10(1000.0), length=10))[1:end-1]
-    tw_idxs = [argmin(abs.(tvec .- tw)) for tw in tws]
-    tws_real = tvec[tw_idxs]
-
-    # Initialize figure
-    fig, ax = plt.subplots(1, 1, figsize=(6, 4), tight_layout=true)
-
-    # Initialize color map
-    my_cmap = plt.matplotlib[:cm][:hot]
-    sm = plt.cm.ScalarMappable(cmap=my_cmap, norm=plt.matplotlib[:colors][:LogNorm](vmin=1, vmax=1000))
-
-    step = 220/10
-    for (i, (tw, tw_idx)) in enumerate(Iterators.zip(tws_real, tw_idxs))
-        ax.plot(tvec[tw_idx:end] .- tw, autocorr_traj[tw_idx:end, tw_idx] ./ autocorr_traj[tw_idx, tw_idx], color=my_cmap(floor(Int, (i-1)*step)))
-    end
-    ax.set_yscale("log")
-    ax.set_xscale("log")
-    ax.set_xlim(1e-1,1e2)
-    ax.set_ylim(autocorr_traj[end,tw_idxs[1]], 1.001)
-    ax.set_xlabel(L"\tau")
-    ax.set_ylabel(L"C(t_w + \tau, t_w)")
-    ax.grid(alpha=.5)
-    clb = plt.colorbar(sm, ax=ax)
-    clb.ax.set_title(L"t_w")
-
-    # Save figure
-    fig.savefig("Corr_N$(N)_K$(K)_J$(J)_D$(D)_tmax$(tmax)_dtMC-$(dt)_dtsave-1e-1_nsim$(nsim)_x0min-$(x0_min)_x0max-$(x0_max).pdf", bbox_inches="tight")
-    fig.savefig("Corr_N$(N)_K$(K)_J$(J)_D$(D)_tmax$(tmax)_dtMC-$(dt)_dtsave-1e-1_nsim$(nsim)_x0min-$(x0_min)_x0max-$(x0_max).png", dpi=300, bbox_inches="tight")
-    plt.close(fig)
-end
-
-N = 1000
-Ks = [4, 10, 100, 999]
-J, D = 1.0, 0.3
-tmax = 6e2
-nsim = 100
-for K in Ks
-    println("Running Bim RRG with K = $K")
-    run_Bim_RRG(N, K, J, D, tmax, nsim)
+    filename = "data/RRG/Corr_Bim_N-$(N)_K-$(K)_J-$(J)_D-$(D)_tmax-$(tmax)_dtMC-$(dt)_dtsave-$(dt_save)_nsim-$(nsim)_x0min-$(x0_min)_x0max-$(x0_max).jld2"
+    jldsave(filename; autocorr_traj, tsave, tidxs)
 end
 
 
-function run_Ferro_RRG(N, K, J, D, tmax, nsim)
+function run_Ferro_RRG(N, K, J, D, tmax, nsim, dt, dt_save)
     rng = Xoshiro(1234)
 
-    x0_min, x0_max = -3.0, 3.0
-    dt = 1e-3
+    x0_min, x0_max = -1.0, 1.0
 
-    tsave = collect(range(0, tmax; step=1e-1))
+    tsave = collect(range(0, tmax; step=dt_save))
 
     # Define ensemble model
     model_ensemble = TwoSpinModelRRG_Ferro(N, K, J, D)
@@ -101,55 +81,114 @@ function run_Ferro_RRG(N, K, J, D, tmax, nsim)
     _, traj_alls, _ = sample_ensemble_2Spin(model_ensemble, x0_min, x0_max, tmax, tsave, nsim; rng=rng, showprogress=true, dt=dt)
 
     # Compute correlation function
-    autocorr_traj, tidxs = compute_autocorr(traj_alls)
+    autocorr_traj, tidxs = compute_autocorr(traj_alls; showprogress=true)
     tvec = tsave[tidxs]
-
+    
     # Save results
     isdir("data") || mkdir("data")
     isdir("data/RRG") || mkdir("data/RRG")
 
-    filename = "data/RRG/Corr_N$(N)_K$(K)_J$(J)_D$(D)_tmax$(tmax)_dtMC-$(dt)_dtsave-1e-1_nsim$(nsim)_x0min-$(x0_min)_x0max-$(x0_max).jld2"
+    filename = "data/RRG/Corr_Ferro_N-$(N)_K-$(K)_J-$(J)_D-$(D)_tmax-$(tmax)_dtMC-$(dt)_dtsave-$(dt_save)_nsim-$(nsim)_x0min-$(x0_min)_x0max-$(x0_max).jld2"
     jldsave(filename; autocorr_traj, tsave, tidxs)
-
-    # Plot correlations
-    tws = collect(10 .^ range(log10(1.0), stop=log10(1000.0), length=10))[1:end-1]
-    tw_idxs = [argmin(abs.(tvec .- tw)) for tw in tws]
-    tws_real = tvec[tw_idxs]
-
-    # Initialize figure
-    fig, ax = plt.subplots(1, 1, figsize=(6, 4), tight_layout=true)
-
-    # Initialize color map
-    my_cmap = plt.matplotlib[:cm][:hot]
-    sm = plt.cm.ScalarMappable(cmap=my_cmap, norm=plt.matplotlib[:colors][:LogNorm](vmin=1, vmax=1000))
-
-    step = 220/10
-    for (i, (tw, tw_idx)) in enumerate(Iterators.zip(tws_real, tw_idxs))
-        ax.plot(tvec[tw_idx:end] .- tw, autocorr_traj[tw_idx:end, tw_idx] ./ autocorr_traj[tw_idx, tw_idx], color=my_cmap(floor(Int, (i-1)*step)))
-    end
-    ax.set_yscale("log")
-    ax.set_xscale("log")
-    ax.set_xlim(1e-1,1e2)
-    ax.set_ylim(autocorr_traj[end,tw_idxs[1]], 1.001)
-    ax.set_xlabel(L"\tau")
-    ax.set_ylabel(L"C(t_w + \tau, t_w)")
-    ax.grid(alpha=.5)
-    clb = plt.colorbar(sm, ax=ax)
-    clb.ax.set_title(L"t_w")
-
-    # Save figure
-    fig.savefig("Corr_N$(N)_K$(K)_J$(J)_D$(D)_tmax$(tmax)_dtMC-$(dt)_dtsave-1e-1_nsim$(nsim)_x0min-$(x0_min)_x0max-$(x0_max).pdf", bbox_inches="tight")
-    fig.savefig("Corr_N$(N)_K$(K)_J$(J)_D$(D)_tmax$(tmax)_dtMC-$(dt)_dtsave-1e-1_nsim$(nsim)_x0min-$(x0_min)_x0max-$(x0_max).png", dpi=300, bbox_inches="tight")
-    plt.close(fig)
 end
 
-N = 1000
-Ks = [4, 10, 100, 999]
+
+#=
+N_dt_s = [(100, 1e-3), (100, 1e-2), (500, 1e-3), (500, 1e-2), (1000, 1e-3), (1000, 1e-2), (2000, 1e-3), (2000, 1e-2)]
+K = 10
 J, D = 1.0, 0.3
-tmax = 6e2
+tmax = 2e2
 nsim = 100
 
-for K in Ks
-    println("Running Ferro RRG with K = $K")
-    run_Ferro_RRG(N, K, J, D, tmax, nsim)
+for (N, dt) in N_dt_s
+    println("Running Bim RRG with N = $N, K = $K, dt=$dt")
+    run_Bim_RRG(N, K, J, D, tmax, nsim, dt)
 end
+
+
+N = 1000
+Ks = [500]
+J, D = 1.0, 0.3
+tmaxs = [2e1]
+nsim = 1000
+dt = 1e-2
+dt_saves = [1e-1]
+
+for (K, tmax, dt_save) in Iterators.zip(Ks, tmaxs, dt_saves)
+    println("Running Bim RRG with N=$N, K = $K")
+    run_Bim_RRG(N, K, J, D, tmax, nsim, dt, dt_save)
+end
+
+
+N = 500
+K = N - 1
+J, D = 1.0, 0.3
+tmax = 2e0
+nsim = 500
+dt = 1e-2
+dt_save = 1e-2
+
+println("Running Bim FC with N=$N")
+run_Bim_RRG(N, K, J, D, tmax, nsim, dt, dt_save)
+=#
+
+N = 1000
+Ks = [10]
+J, D = 1.0, 0.3
+tmaxs = [2e1]
+nsim = 500
+dt = 1e-2
+dt_saves = [1e-1]
+
+for (K, tmax, dt_save) in Iterators.zip(Ks, tmaxs, dt_saves)
+    println("Running Ferro RRG with N=$N, K = $K")
+    run_Ferro_RRG(N, K, J, D, tmax, nsim, dt, dt_save)
+end
+#=
+N = 500
+K = N - 1
+J, D = 1.0, 0.3
+tmax = 2e1
+nsim = 500
+dt = 1e-2
+dt_save = 1e-2
+
+println("Running Ferro FC with N=$N")
+run_Ferro_RRG(N, K, J, D, tmax, nsim, dt, dt_save)
+
+
+function run_GECaM_Bim_RRG(K, J, D, dt, T)
+    # Create tvec
+    tvec_G = [dt * n for n in 0:T]
+    tmax = T * dt
+    
+    # Compute GECaM
+    C_G, R_G, Ch_G, Rh_G, mu_G = integrate_2spin_Bim_RRG(K, J, D, dt, T; showprogress=true)
+    
+    # Save results
+    filename = "data/RRG/Corr_GECaM_K-$(K)_J-$(J)_D-$(D)_tmax-$(tmax)_dt-$(dt).jld2"
+    jldsave(filename; tvec_G, C_G, R_G, Ch_G, Rh_G, mu_G)
+end
+
+Ks = [4, 10, 100]
+J, D = 1.0, 0.3
+tmax = 2e2
+dt = 2e-2
+T = ceil(Int, tmax / dt)
+
+for K in Ks
+    println("Running GECaM for K=$K")
+    run_GECaM_Bim_RRG(K, J, D, dt, T)
+end
+
+
+N, K = 1000, 5
+J, D = 1.0, 0.3
+tmax = 2e1
+nsim = 10000
+dt = 1e-2
+dt_save = 1e-1
+ilist = [1, 2, 1000]
+
+run_Bin_RRG_single_nodes(N, K, J, D, tmax, nsim, dt, dt_save, ilist)
+=#
